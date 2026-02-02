dc077d46af44d6a1c368e6e067ebc87e7a55203e
Said <pm5581287@gmail.com>
2026-02-02 23:50:44 +0300

    add search_product function

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.main.views import IndexView, ProductaDetailView
+from apps.main.views import IndexView, ProductaDetailView, search_products
 
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('product/<slug:slug>/', ProductaDetailView.as_view(), name='product-detail')
+    path('product/<slug:slug>/', ProductaDetailView.as_view(), name='product-detail'),
+    path('searh-products/', search_products, name='search-products')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView, DetailView
 from .models import Product, ProductSize
 from django.template.response import TemplateResponse
-
+from django.shortcuts import render
 
 
 
@@ -9,9 +9,10 @@ class IndexView(TemplateView):
   template_name = 'main/index.html'
 
   def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["products"] = Product.objects.all()
-      return context
+    context = super().get_context_data(**kwargs)
+    context["products"] = Product.objects.all()
+    return context
+
 
   def get(self, request, *args, **kwargs):
     context = self.get_context_data(**kwargs)
@@ -36,3 +37,18 @@ class ProductaDetailView(DetailView):
     if request.headers.get('HX-Request'):
       return TemplateResponse(request, 'main/partials/product_detail.html', context)
     return TemplateResponse(request, self.template_name, context)
+
+
+def search_products(request):
+  query = request.GET.get('q')
+
+  if query:
+    products = Product.objects.filter(title__icontains=query)
+  else:
+    products = Product.objects.all()
+
+  context={
+    'products': products
+    }
+
+  return render(request, 'main/components/products.html', context)
